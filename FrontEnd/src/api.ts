dd81d43d58677e88fcc88b2f82e9c6ee6b8ceafd
samansayad93 <sayadhelli10@gmail.com>
2025-12-09 03:50:09 +0330

    fix the path

--- FrontEnd/src/api.ts
+++ FrontEnd/src/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  //baseURL: "https://api.smartparking.niflheimdevs.ir/v1",
-  baseURL: "http://localhost:8080/v1",
+  baseURL: "https://api.smartparking.niflheimdevs.ir/v1",
   headers: {
     "Content-Type": "application/json",
   },
